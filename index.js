dd1c7d4fdb66f9e877a4f7bcfc9daa72abab95dc
Yoshihiko Yamada <oh-yamada@MBP.local>
2017-01-25 17:31:11 +0900

    引数を除去

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 function loopbackModelConfigAddMixinPath(filename, path) {
-  var fs = require('fs');
-  var json = JSON.parse(fs.readFileSync(filename));
+  let fs = require('fs');
+  let json = JSON.parse(fs.readFileSync(filename));
   if(json._meta.mixins.indexOf(path) == -1) {
     json._meta.mixins.push(path);
   }
   fs.writeFile(filename, JSON.stringify(json, null, 2));
 }
 
-if(process.argv.length != 4) {
-  console.log('ex: $ node index.js "./../server/model-config.json" "../node_modules/loopback-ds-timestamp-mixin"');
-  process.exit(1);
-}
+loopbackModelConfigAddMixinPath('../../server/model-config.json', '../node_modules/loopback-ds-timestamp-mixin');
 
-loopbackModelConfigAddMixinPath(process.argv[2], process.argv[3]);
-
- 
